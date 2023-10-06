98f41c365dd39ac22d1998b4637f748e7df82dbe
taswakil <designerfairytasneem@gmail.com>
2023-10-05 23:55:14 -0500

    Update scripts.js
    add js file and include functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Welcome to my portfolio site")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+  
